c04544d9691359c7e3bce713c7adbbe1cd969eeb
Paul H Cassidy <qawarrior@gmail.com>
2017-10-29 16:32:29 -0700

    setting changes into history

--- backup.cmd
+++ backup.cmd
@@ -1,2 +1,5 @@
+git add --all
+git commit -m "setting changes into history"
+git push
 restic backup -r G:\RESTIC_BACKUP\ C:\work\go\src\github.com\qawarrior\serve-nt\
 restic forget -r G:\RESTIC_BACKUP\ --keep-last 2 --prune
